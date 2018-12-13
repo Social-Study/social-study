4b15c15f6fe046e8d73559140bef181f0bb988d6
Dylan Dougherty <dyldougherty@gmail.com>
2018-12-12 19:38:15 -0500

    - added flash card flip animations - added navigation with arrow keys - added flip with spacebar - cleaned up code - fixed issue with component name - modified TODO

--- src/views/FlashcardStudy.vue
+++ src/views/FlashcardStudy.vue
@@ -1,25 +1,37 @@
 <template>
-  <div class="content-container">
+  <div 
+  class="content-container"
+  @keyup.space="flipcard()"
+  >
 
     <h1>Software Engineering Flashcards</h1>
-    <div class="page-content">
+    <div 
+    class="page-content"
+    >
       <button
         @click="prevCard"
         class="btn btn-action btn-success btn-lg s-circle"
       > <i class="icon icon-arrow-left"></i>
       </button>
-      <div class="flashcard-container">
-        <div
+      <div class = "flashcard-container">
+        <flashcard
           class="flashcard"
-          @click="flipcard"
+          :style="{backgroundColor: cardColor}"
+          :pose="isFlipping ? 'shrink' : 'grow' "
+          @click.native="flipcard"
         >
-          <h1 class="flashcard-term">{{currentContent}}</h1>
-        </div>
+          <flashcardContent
+            class="flashcard-content"
+            :pose="isFlipping ? 'shrinkContent' : 'growContent' "
+            >
+            {{currentContent}}
+          </flashcardContent>
+        </flashcard>
       </div>
-      <button
-        @click="nextCard"
-        class="btn btn-action btn-success btn-lg s-circle"
-      > <i class="icon icon-arrow-right"></i>
+      <button 
+        @click="nextCard" 
+        class="btn btn-action btn-success btn-lg s-circle">  
+        <i class="icon icon-arrow-right"></i>
       </button>
     </div>
     <h1> {{cardIndex + 1}} / 5</h1>
@@ -29,17 +41,46 @@
 
 <script>
 // import firebase, { db, FirebaseConsts } from "@/firebaseConfig";
+import posed from 'vue-pose';
+import { setTimeout } from 'timers';
 
 export default {
-  name: "dashboard",
+  name: "flashcardStudy",
+  components: {
+    flashcard: posed.div({
+      shrink: { 
+        height: 0,
+        transition: { duration: 200}
+      },
+      grow: { 
+        height: 300,
+        transition: { duration: 200 }
+      }
+    }),
+    flashcardContent: posed.h1({
+      shrinkContent: {
+        opacity: 0,
+        transition: { duration: 10 }
+      },
+      growContent: {
+        opacity: 1,
+        transition: {delay: 100}
+      }
+    })
+  },
+
   data: function() {
     return {
+      isFlipping: false,
       user: null,
       currentContent: String,
       cardIndex: 0,
       flipped: false,
       termList: [],
       definitionList: [],
+      cardColor: '#E7E7E7',
+
+
 
       // hard-coded flashcard deck for testing
       flashcardDeck: [
@@ -60,35 +101,73 @@ export default {
   methods: {
     //flips the current card
     flipcard() {
-      if (this.flipped) {
-        this.getCurrentContent();
-        this.flipped = false;
-      } else {
-        this.getCurrentContent();
-        this.flipped = true;
+      this.isFlipping = true;
+      const self = this;
+      setTimeout(function(){ 
+      self.isFlipping = false;
+      //definition side
+      if (self.flipped) {
+        self.getCurrentContent();
+        self.flipped = false;
+        self.cardColor = '#E7E7E7';
+      } 
+      //term side
+      else {
+        self.getCurrentContent();
+        self.flipped = true;
+        self.cardColor = '#c5c5c5';
       }
-    },
-    //incrases the current card index and updates the displayed content
+    }, 100);  
+
+  },
+    //increments the current card index and updates the displayed content
     nextCard() {
       if (this.cardIndex < this.termList.length - 1) {
         this.cardIndex++;
         this.currentContent = this.termList[this.cardIndex];
+        this.flipped = false;
+        this.cardColor = '#E7E7E7';
       }
     },
+    //decrements the current card index and updates the displayed info
     prevCard() {
       if (this.cardIndex > 0) {
         this.cardIndex--;
+        this.flipped = false;
         this.currentContent = this.termList[this.cardIndex];
+        this.cardColor = '#E7E7E7';
       }
     },
-    //loads the content that should be shown into the currentContent variable
+    //loads the content that should be shown into the currentContent variable based on flipped variable
     getCurrentContent() {
       if (this.flipped) {
         this.currentContent = this.termList[this.cardIndex];
       } else {
         this.currentContent = this.definitionList[this.cardIndex];
       }
+    },
+    //calls the correct function based on key press
+    keyPressed(event) {
+      switch(event.which){
+        case 32:
+          this.flipcard();
+          break;
+        case 37:
+          this.prevCard();
+          break;
+        case 39:
+          this.nextCard();
+          break;
+      }
     }
+  },
+  //creates the key event listener
+  beforeMount() {
+    window.addEventListener('keyup', this.keyPressed);
+  },
+  //destroys the key event listener
+  beforeDestroy () {
+    window.removeEventListener('keyup', this.keyPressed);
   },
   //load all the data from the flashcard deck into arrays
   //set the first card's content
@@ -122,18 +201,18 @@ export default {
 }
 
 .flashcard {
-  min-height: 300px;
+  height: 300;
   display: flex;
   flex-direction: column;
   justify-content: center;
-  background-color: #e7e7e7;
+  background-color: #c5c5c5;
   border-radius: 10px;
   width: 500px;
   box-shadow: $shadow-heavy;
 }
 
-.flashcard-term {
-  font-size: 50px;
+.flashcard-content {
+  font-size: 300%;
   -webkit-touch-callout: none;
   -webkit-user-select: none;
   -khtml-user-select: none;
