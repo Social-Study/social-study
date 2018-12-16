a3dff03ecc8991d1a02e298a0444d477807e7fa9
Evan Buss <evan.buss28@gmail.com>
2018-12-16 13:05:06 -0500

    Added new vue-notifcations package. - These will be used to display to the user when an event has occured that they should be aware of

--- src/main.js
+++ src/main.js
@@ -11,6 +11,9 @@ import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 import locale from "element-ui/lib/locale/lang/en";
 
+import Notification from "vue-notification";
+Vue.use(Notification);
+
 Vue.use(ElementUI, { locale });
 
 Vue.config.productionTip = false;
