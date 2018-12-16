e5452615c60d2d15c6f09fdc614768a22bff18f7
Evan Buss <evan.buss28@gmail.com>
2018-12-15 22:12:15 -0500

    Config Changes

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +6,11 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.syncMembers = functions.firestore
+  .document("/study-groups/{groupID}/members/members")
+  .onWrite((change, context) => {
+    console.log("Sync Members Cloud Function Called");
+
+    // const newValue = change.after.data();
+  });
